67c2f2531b771feb69c5c370836a911d4caac1ec
Evan Shimizu <falindrith@gmail.com>
2014-09-16 14:47:26 -0400

    Beginning work on snapshots
    Fixing a few bugs with new file names

--- Lumiverse/source/LumiverseShowControl/LumiverseShowControl.h
+++ Lumiverse/source/LumiverseShowControl/LumiverseShowControl.h
@@ -1,7 +1,8 @@
 // Header that includes all the files in the LumiverseShowControl project
 
-#include <Cue.h>
-#include <CueList.h>
-#include <Layer.h>
-#include <Programmer.h>
-#include <Playback.h>
+#include "Cue.h"
+#include "CueList.h"
+#include "Layer.h"
+#include "Programmer.h"
+#include "Playback.h"
+#include "Snapshot.h"
