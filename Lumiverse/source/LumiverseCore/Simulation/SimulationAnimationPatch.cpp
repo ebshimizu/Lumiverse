44487442e45d5b54b0b84dc5cf23906f3e9876ae
Chelsea21 <chenxil@andrew.cmu.edu>
2014-09-26 11:26:26 -0400

    Added missing files for SimulationAnimationPatch.

---
+++ Lumiverse/source/LumiverseCore/Simulation/SimulationAnimationPatch.cpp
@@ -0,0 +1,336 @@
+
+#include "SimulationAnimationPatch.h"
+
+#include <sstream>
+
+namespace Lumiverse {
+
+// Uses chrono::system_clock::from_time_t(0) as an invalid value.
+SimulationAnimationPatch::SimulationAnimationPatch(const JSONNode data)
+: m_worker(NULL), m_startPoint(chrono::system_clock::from_time_t(0)),
+    m_mode(SimulationAnimationMode::STOPPED) {
+    // TODO: type for frame manager
+	m_mem_frameManager = new ArnoldMemoryFrameManager();
+	m_file_frameManager = NULL;
+    loadJSON(data);
+}
+    
+SimulationAnimationPatch::~SimulationAnimationPatch() {
+    delete m_mem_frameManager;
+	delete m_file_frameManager;
+
+    // If close() hasn't been called, closes here.
+    if (m_worker != NULL)
+        close();
+}
+
+void SimulationAnimationPatch::loadJSON(const JSONNode data) {
+	SimulationPatch::loadJSON(data);
+
+	string patchName = data.name();
+
+	// Load options for raytracer application. (window, ray tracer, filter)
+	JSONNode::const_iterator i = data.begin();
+
+	while (i != data.end()) {
+		std::string nodeName = i->name();
+
+		if (nodeName == "frameDirectory") {
+			JSONNode fileName = *i;
+			m_file_frameManager = new ArnoldFileFrameManager(fileName.as_string());
+		}
+
+		i++;
+	}
+}
+
+void SimulationAnimationPatch::init() {
+    SimulationPatch::init();
+
+	// Cleans hooks to old callbacks
+	m_onFinishedFunctions.clear();
+}
+    
+void SimulationAnimationPatch::update(set<Device *> devices) {
+	// Doesn't respond if it's stopped.
+    if (m_mode == SimulationAnimationMode::STOPPED)
+        return ;
+    
+    FrameDeviceInfo frame;
+	
+	// Fulls time and mode for frame info.
+	createFrameInfoHeader(frame);
+
+    // Checks if an update is needed.
+    bool rerender_req = isUpdateRequired(devices);
+
+    // There's no need to send this frame
+    if (!rerender_req)
+        return ;
+    
+    for (Device *d : devices) {
+		// Checks if the device is connect to this patch
+		if (m_lights.count(d->getId()) > 0 &&
+			m_lights[d->getId()]->rerender_req) {
+			// Makes copy of this device
+			Device *d_copy = new Device(*d);
+			frame.devices.insert(d_copy);
+		}
+    }
+
+    std::stringstream ss;
+    ss << "Sent new frame: " << frame.time << "(" << frame.mode << ")";
+    Logger::log(LDEBUG, ss.str());
+
+	// Interrupts the current rendering if in the interactive mode.
+    if (m_mode == SimulationAnimationMode::INTERACTIVE ||
+		m_mode == SimulationAnimationMode::RECORDING) {
+		interruptRender();
+    }
+    
+	// Enqueues.
+	enqueueFrameInfo(frame);
+
+	// A flag cleared means the task has been indeed inserted
+	clearUpdateFlags();
+}
+
+void SimulationAnimationPatch::enqueueFrameInfo(const FrameDeviceInfo &frame) {
+	// Since there aren't many competitions (worker runs slowly),
+	// it's okay to just use a coarse lock.
+	m_queue.lock();
+	// Checks if start point was reset during this update call.
+	if (m_startPoint != chrono::system_clock::from_time_t(0))
+		m_queuedFrameDeviceInfo.push_back(frame);
+	m_queue.unlock();
+}
+
+void SimulationAnimationPatch::createFrameInfoHeader(FrameDeviceInfo &frame) {
+	chrono::time_point<chrono::system_clock> current = chrono::system_clock::now();
+
+	// Sets the start point to the moment when update is called for the first time
+	if (m_startPoint == chrono::system_clock::from_time_t(0))
+		m_startPoint = current;
+
+	frame.time = chrono::duration_cast<chrono::milliseconds>(current - m_startPoint).count();
+
+	// A new Interactive frame indicates the end of Rendering tasks.
+	// Note that all Rendering frames are created by the worker thread.
+	if (m_mode == SimulationAnimationMode::RENDERING ||
+		m_mode == SimulationAnimationMode::INTERACTIVE)
+		frame.mode = SimulationAnimationMode::INTERACTIVE;
+	else if (m_mode == SimulationAnimationMode::RECORDING)
+		frame.mode = SimulationAnimationMode::RECORDING;
+
+}
+
+void SimulationAnimationPatch::rerender() {
+	// Sets all rerendering flags to true to preserve the first frame
+	if (m_lights.size() > 0)
+		for (auto light : m_lights)
+			m_lights[light.first]->rerender_req = true;
+
+	// When patch is working, makes sure the request be processed
+	while (m_lights.begin()->second->rerender_req &&
+		m_mode != SimulationAnimationMode::STOPPED) ;
+}
+
+void SimulationAnimationPatch::close() {
+    // Won't close immediately
+    // Sends end signal to worker
+    FrameDeviceInfo frame;
+	chrono::time_point<chrono::system_clock> current = chrono::system_clock::now();
+	frame.time = chrono::duration_cast<chrono::milliseconds>(current - m_startPoint).count();
+    frame.mode = SimulationAnimationMode::STOPPED;
+
+    m_queue.lock();
+    m_queuedFrameDeviceInfo.push_back(frame);
+    m_queue.unlock();
+
+    // Sets to interactive mode, so worker can immediately process
+    // the end info.
+    startInteractive();
+    
+    // Waits until worker finishes its job
+    if (m_worker != NULL)
+        m_worker->join();
+    m_worker = NULL;
+    
+    m_mode = SimulationAnimationMode::STOPPED;
+    
+    // Close arnold interface
+    SimulationPatch::close();
+}
+
+ArnoldFrameManager *SimulationAnimationPatch::getFrameManager() const {
+    return m_mem_frameManager;
+}
+
+void SimulationAnimationPatch::reset() {
+    // We want to block both worker and main thread during resetting
+    m_queue.lock();
+    m_mode = SimulationAnimationMode::INTERACTIVE;
+
+    // Resets start point to init.
+    m_startPoint = chrono::system_clock::from_time_t(0);
+    
+    // Clears queue.
+    for (FrameDeviceInfo d : m_queuedFrameDeviceInfo) {
+        d.clear();
+    }
+    
+    m_queuedFrameDeviceInfo.clear();
+    
+    // Just in case that close()'s already been called.
+    if (m_worker != NULL) {
+        // Manully interrupts worker here.
+        interruptRender();
+    }
+    else {
+        // Reset() can set a closed SimulationAnimationPatch to its initial state.
+        m_worker = new std::thread(&SimulationAnimationPatch::workerLoop, this);
+    }
+    
+	// Resets the frame manager to the beginning of current video.
+    m_mem_frameManager->reset();
+	if (m_file_frameManager)
+		m_file_frameManager->reset();
+    
+    m_queue.unlock();
+}
+
+void SimulationAnimationPatch::stop() {
+    m_mode = SimulationAnimationMode::STOPPED;
+}
+
+void SimulationAnimationPatch::endRecording() {
+    if (m_queuedFrameDeviceInfo.size() > 0)
+        m_mode = SimulationAnimationMode::RENDERING;
+    else
+        m_mode = SimulationAnimationMode::INTERACTIVE;
+}
+
+int SimulationAnimationPatch::addFinishedCallback(FinishedCallbackFunction func) {
+    size_t id = m_onFinishedFunctions.size();
+    m_onFinishedFunctions[id] = func;
+    
+    return id;
+}
+    
+void SimulationAnimationPatch::deleteFinishedCallback(int id) {
+    if (m_onFinishedFunctions.count(id) > 0) {
+        m_onFinishedFunctions.erase(id);
+    }
+}
+    
+//============================ Worker Code =========================
+
+void SimulationAnimationPatch::onWorkerFinished(){
+	onRecording();
+	m_mode = SimulationAnimationMode::INTERACTIVE;
+
+    for (auto func : m_onFinishedFunctions) {
+        func.second();
+    }
+}
+    
+void SimulationAnimationPatch::workerRender(FrameDeviceInfo frame) {
+	std::stringstream ss;
+	ss << "Received new frame: " << frame.time << "(" << frame.mode << ")";
+	Logger::log(LDEBUG, ss.str());
+
+	updateLight(frame.devices);
+	bool success = SimulationPatch::renderLoop();
+
+	// Dumps only when the image was rendered successfully for rendering.
+	// If the worker was reset while rendering, doesn't dump.
+	if (success && frame.mode == SimulationAnimationMode::RENDERING) {
+		m_mem_frameManager->dump(frame.time, getBufferPointer(),
+			getWidth(), getHeight());
+		if (m_file_frameManager)
+			m_file_frameManager->dump(frame.time, getBufferPointer(),
+			getWidth(), getHeight());
+	}
+}
+
+void SimulationAnimationPatch::workerLoop() {
+    FrameDeviceInfo frame;
+
+    while(1) {
+		// Releases the lock immediately if the queue is still empty.
+		while (frame.time < 0) {
+			if (m_mode == SimulationAnimationMode::STOPPED)
+				continue;
+
+			m_queue.lock();
+			if (m_queuedFrameDeviceInfo.size() > 0) {
+				if (m_mode == SimulationAnimationMode::RECORDING) {
+					// Clears irrelated info
+					std::vector<FrameDeviceInfo>::iterator i;
+					for (i = m_queuedFrameDeviceInfo.begin();
+						 i != m_queuedFrameDeviceInfo.end() &&
+						 i->mode == SimulationAnimationMode::INTERACTIVE; i++) {
+						i->clear();
+					}
+					m_queuedFrameDeviceInfo.erase(m_queuedFrameDeviceInfo.begin(), i);
+                
+					// Deep copy (since we're going to clear it)
+					for (i = m_queuedFrameDeviceInfo.begin();
+						 i != m_queuedFrameDeviceInfo.end(); i++) {
+						// Copies the last recording info to do preview
+						if (i->mode == SimulationAnimationMode::RECORDING) {
+							frame.copyByValue(*i);
+							// Waits to be rendered with higher sampling rate
+							i->mode = SimulationAnimationMode::RENDERING;
+						}
+					}
+				}
+				else if (m_mode == SimulationAnimationMode::RENDERING) {
+					// No frame would be skipped.
+					frame = m_queuedFrameDeviceInfo[0];
+					m_queuedFrameDeviceInfo.erase(m_queuedFrameDeviceInfo.begin());
+				}
+				else if (m_mode == SimulationAnimationMode::INTERACTIVE) {
+					frame = m_queuedFrameDeviceInfo.back();
+					for (FrameDeviceInfo &info : m_queuedFrameDeviceInfo) {
+						if (info.time != m_queuedFrameDeviceInfo.back().time)
+							info.clear();
+					}
+					m_queuedFrameDeviceInfo.clear();
+				}
+			}
+			// No more frames for rendering
+			else if (m_mode == SimulationAnimationMode::RENDERING) {
+				onWorkerFinished();
+			}
+			m_queue.unlock();
+
+			// The frame is valid.
+			if (frame.time >= 0) {
+				if (frame.mode == SimulationAnimationMode::STOPPED) {
+					m_mode = SimulationAnimationMode::STOPPED;
+					Logger::log(INFO, "Worker stopped...");
+
+					return ;
+				}
+				else if (frame.mode == SimulationAnimationMode::RECORDING) {
+					onRecording();
+				}
+				else if (frame.mode == SimulationAnimationMode::RENDERING) {
+					onRendering();
+				}
+				// Finished recording work
+				else if (frame.mode == SimulationAnimationMode::INTERACTIVE &&
+						 (m_mode == SimulationAnimationMode::RENDERING)) {
+					onWorkerFinished();
+				}
+			}
+		}
+
+		// Releases copies of devices.
+		workerRender(frame);
+		frame.clear();
+    }
+}
+
+}
---
+++ Lumiverse/source/LumiverseCore/Simulation/SimulationAnimationPatch.h
@@ -0,0 +1,280 @@
+/*! \file SimulationAnimationPatch.h
+* \brief Subclass of ArnoldPatch to render frames of an animation.
+*/
+#ifndef _SimulationAnimationPATCH_H_
+#define _SimulationAnimationPATCH_H_
+
+#pragma once
+
+#include "LumiverseCoreConfig.h"
+#ifdef USE_ARNOLD
+
+#include "../lib/libjson/libjson.h"
+#include "SimulationPatch.h"
+#include "ArnoldMemoryFrameManager.h"
+#include "ArnoldFileFrameManager.h"
+
+#include <thread>
+#include <chrono>
+#include <iostream>
+
+namespace Lumiverse {
+	/*! \brief Four working modes of SimulationAnimationPatch. 
+	* INTERACTIVE: Patch renders the latest sent frame with preview sampling rate (camera).
+	* RECORDING: Patch renders the preview scene with similar behavior as INTERACTIVE,
+	* and it also creates a duplicate for each frame, which is used later for rendering.
+	* RENDERING: Renders the frames with rendering sampling rate.
+	* STOPPED: Patch doesn't respond to any input. This state is usually used for playing video.
+	*/
+  enum SimulationAnimationMode {
+      INTERACTIVE, RECORDING, RENDERING, STOPPED
+  };
+    
+  /*! \brief The state info for worker thread. */
+  struct FrameDeviceInfo {
+      // The time point of this frame.
+      // It's actually the duration counted from the update is first time
+      // called.
+      time_t time;
+      // Copies for devices connected to this patch.
+      std::set<Device *> devices;
+      SimulationAnimationMode mode;
+
+      /*! \brief Constructor. */
+      FrameDeviceInfo() : time(-1), mode(SimulationAnimationMode::INTERACTIVE) { }
+
+      /*! \brief Releases the copies for devices. */
+      void clear() {
+		  time = -1;
+
+		  for (Device *d : devices) {
+			  if (d != NULL)
+			  delete d;
+			  d = NULL;
+		  }
+		  devices.clear();
+      }
+      
+	  /*! \brief Deep copy. */
+      void copyByValue(const FrameDeviceInfo &other) {
+          time = other.time;
+          mode = other.mode;
+          for (Device *d : other.devices) {
+              devices.insert(new Device(d));
+          }
+      }
+  };
+    
+  /*!
+  * \brief A subclass of ArnoldPatch. 
+  * Instead of interrupting the worker thread every time a new rendering 
+  * task is received, this class keeps all requests in a queue. A worker
+  * thread grasps tasks and dumps frame buffer to a ArnoldFrameManager.
+  *  
+  * \sa ArnoldPatch, ArnoldFrameManager
+  */
+  class SimulationAnimationPatch : public SimulationPatch
+  {
+  public:
+    /*!
+    * \brief Constructs a SimulationAnimationPatch object.
+    */
+    SimulationAnimationPatch() : m_worker(NULL), 
+	  m_startPoint(std::chrono::system_clock::from_time_t(0)),
+	  m_mem_frameManager(NULL), m_file_frameManager(NULL), m_mode(SimulationAnimationMode::STOPPED) { }
+
+    /*!
+    * \brief Constructs ArnoldPatch from JSON data.
+    *
+    * \param data JSONNode containing the SimulationAnimationPatch object data.
+    */
+    SimulationAnimationPatch(const JSONNode data);
+
+    /*!
+    * \brief Destroys the object.
+    */
+    virtual ~SimulationAnimationPatch();
+
+    /*!
+    * \brief Initializes Arnold with function of its parent class and
+    * starts a worker thread.
+    */
+    virtual void init() override;
+
+    /*!
+    * \brief Starts recording.
+    * Main thread starts to send frame labeled as RECORDING info to worker.
+    */
+    void startRecording() { 
+		m_mem_frameManager->clear();
+
+		// Overwrite existing frames instead of clearing all
+		if (m_file_frameManager)
+			m_file_frameManager->reset();
+
+		m_mode = SimulationAnimationMode::RECORDING; 
+	}
+    
+	/*!
+	* \brief Ends recording.
+	* Main thread stops to send frame labeled as RECORDING info to worker.
+	* It starts to send INTERACTIVE frame instead.
+	*/
+    void endRecording();
+      
+    /*!
+    * \brief Starts interactive mode.
+    * Worker thread can get interrupted. It always takes the most fresh info.
+    */
+    void startInteractive() { m_mode = SimulationAnimationMode::INTERACTIVE; }
+      
+	/*!
+	* \brief Returns the mode/state in which the patch is.
+	*
+	* \return The mode/state.
+	*/
+    SimulationAnimationMode getMode() { return m_mode; }
+      
+    /*!
+    * \brief Gets the type of this object.
+    *
+    * \return String containing "SimulationAnimationPatch"
+    */
+    virtual string getType() { return "SimulationAnimationPatch"; }
+
+    /*!
+    * \brief Updates the rendering queue given the list of devices
+    * in the rig.
+    *
+    * Before enqueuing the rendering info, main thread checks if
+    * there is any parameter or metadata changed during last update
+    * interval. It only adds a new request when it's truly necessary.
+    */
+	virtual void update(set<Device *> devices) override;
+
+	/*!
+	* \brief Manually schedule a re-rendering and make sure the task be inserted into queue.
+	*
+	* This new rendering task may not be done immediately, but it will get inserted.
+	*/
+	virtual void rerender();
+
+    /*!
+    * \brief Waits for the worker thread and closes the Arnold session.
+    *
+    * The main thread sends a special frame info at the beginning of
+    * this function. (a info with devices list only containning a NULL)
+    * Then the thread would wait to join the worker thread. After all
+    * there are done, closes the arnold session as the parent class.
+    */
+	virtual void close() override;
+
+    /*!
+    * \brief Returns the ArnoldFrameManager to reconstruction the 
+    * animation.
+    *
+    * \return The ArnoldFrameManager object containning all the frame
+    * and their corresponding time point.
+    */
+    ArnoldFrameManager *getFrameManager() const;
+    
+    /*!
+    * \brief Resets the object to its initial state.
+    *
+    * Including resetting start point, clearing frame manager, interrupting worker and clearing worker's queue.
+    */
+    void reset();
+      
+	/*!
+	* \brief Stops the patch.
+	*
+	* The main thread would stop responding to new requests and the worker thread would be joined.
+	*/
+    void stop();
+      
+    // Callbacks
+    typedef function<void()> FinishedCallbackFunction;
+      
+    /*!
+    * \brief Registers a callback function for parameter changed event.
+    *
+    * All registered functinos would be called when a parameter is changed
+    * by Device::setParam and Device::reset function.
+    * \param func The callback function.
+    * \return The int id for the registered function.
+    * \sa addMetadataChangedCallback(DeviceCallbackFunction func)
+    */
+    int addFinishedCallback(FinishedCallbackFunction func);
+      
+    /*!
+    * \brief Deletes a registered callback for parameter change
+    *
+    * \param id The id returned when the callback is registered
+    * \sa addParameterChangedCallback(DeviceCallbackFunction func)
+    */
+    void deleteFinishedCallback(int id);
+
+  protected:
+	/*!
+	* \brief Loads data from a parsed JSON object
+	* \param data JSON data to load
+	*/
+	virtual void loadJSON(const JSONNode data) override;
+
+	/*!
+	* \brief Worker loop.
+	*
+	* Dequeues a new task. Sets the light parameters and renders. Dumps
+	* the frame buffer. The loop ends when an end info is received.
+	*/
+	virtual void workerLoop();
+
+	/*!
+	* \brief Helper to call all the registered callbacks for rendering finished event.
+	*/
+	virtual  void onWorkerFinished();
+
+	virtual void onRecording() { }
+
+	virtual void onRendering() { }
+
+	virtual void workerRender(FrameDeviceInfo frame);
+
+	void createFrameInfoHeader(FrameDeviceInfo &frame);
+
+	void enqueueFrameInfo(const FrameDeviceInfo &frame);
+
+    // The worker thread.
+    std::thread *m_worker;
+
+    // The lock for the task queue.
+    std::mutex m_queue;
+
+    // The task queue.
+    std::vector<FrameDeviceInfo> m_queuedFrameDeviceInfo;
+
+    // The start point for time points in FrameDeviceInfo.
+    // It's the moment when update function is called for the first
+    // time.
+    std::chrono::time_point<std::chrono::system_clock> m_startPoint;
+
+    // The ArnoldFrameManager object. Used to store frame buffers.
+    ArnoldMemoryFrameManager *m_mem_frameManager;
+	ArnoldFileFrameManager *m_file_frameManager;
+      
+    /*! \brief Indicates the mode of SimulationAnimationPatch.
+    */
+    SimulationAnimationMode m_mode;
+      
+	/*! \brief The list for callback functions.
+	*/
+    map<int, FinishedCallbackFunction> m_onFinishedFunctions;
+
+	private: 
+  };
+    
+}
+
+#endif
+
+#endif
